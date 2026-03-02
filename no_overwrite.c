1b3467427f4feddb97ec120f0d95ded4079a57f3
kernelkiranerys <J.Edwards2529@student.leedsbeckett.ac.uk>
2026-03-02 14:58:54 +0000

    no overwrite simplification

--- no_overwrite.c
+++ no_overwrite.c
@@ -1,30 +1,15 @@
-#include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int main(int argc, char *argv[]) {
-	const char *path = "demo_no_overwrite.txt";
-
-	if (argc > 1) {
-		path = argv[1];
-	}
-
-	errno = 0;
-	FILE *fp = fopen(path, "wx");
+int main(void) {
+	FILE *fp = fopen("test.txt", "wx");
+	
 	if (fp == NULL) {
-		fprintf(stderr, "Failed to create '%s' with mode 'wx'.\n", path);
 		perror("fopen");
-		return EXIT_FAILURE;
+		return 1;
 	}
 
-	fputs("Created without overwriting an existing file.\n", fp);
-
-	if (fclose(fp) != 0) {
-		perror("fclose");
-		return EXIT_FAILURE;
-	}
-
-	printf("Created '%s' successfully.\n", path);
-	return EXIT_SUCCESS;
+	fputs("File created successfully.\n", fp);
+	fclose(fp);
+	
+	return 0;
 }
